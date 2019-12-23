93a3524fab0211104bb1cf6c226d16ab94e49ebc
Julian Rodriguez Calderon <jrodriguezc@gmail.com>
2019-12-23 15:51:42 -0500

    - add soap_ws_test.feature file with scenarios to test a soap ws - add StepDefinitionsSOAPTes.java file with test a SOPA ws

---
+++ API-Test/src/test/java/stepdefinitions/StepsDefinitionsSOAPTest.java
@@ -0,0 +1,58 @@
+package stepdefinitions;
+
+import apitest.utilities.APIConstants;
+import apitest.ws.CountryInfoServiceLocator;
+import apitest.ws.CountryInfoServiceSoapBindingStub;
+import apitest.ws.CountryInfoServiceSoapType;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import javax.xml.rpc.ServiceException;
+import java.rmi.RemoteException;
+
+public class StepsDefinitionsSOAPTest
+{
+    private CountryInfoServiceSoapBindingStub stub;
+    private CountryInfoServiceLocator locator;
+    private String capitalCity;
+
+    @Before
+    public void setup(){
+        this.locator = new CountryInfoServiceLocator();
+        locator.setCountryInfoServiceSoapEndpointAddress(APIConstants.WS_SOAP_URL_ENDPOINT);
+    }
+
+    @Given("^I connect to the SOAP endpoint$")
+    public void i_connect_to_the_SOAP_endpoint()
+    {
+        if(stub == null){
+            try {
+                this.stub = (CountryInfoServiceSoapBindingStub) locator.getPort(CountryInfoServiceSoapType.class);
+            } catch (ServiceException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @When("^I fill a ISO Country code with (.*)$")
+    public void i_fill_a_ISO_Country_code_with(String isoCode)
+    {
+        try {
+            this.capitalCity = stub.capitalCity(isoCode);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Then("^I validate if the capital name (.*) is correct$")
+    public void i_validate_if_the_capital_name_is_correct(String expectedCapital)
+    {
+        Assert.assertEquals(expectedCapital,this.capitalCity);
+    }
+
+
+
+}
---
+++ API-Test/src/test/resources/features/soap_ws_test.feature
@@ -0,0 +1,15 @@
+# new feature
+# Tags: optional
+
+Feature: Test a SOAP Web Service using Cucumber
+
+  Scenario Outline: Verify the capital of a list of countries using ISO code
+    Given I connect to the SOAP endpoint
+    When I fill a ISO Country code with <isoCode>
+    Then I validate if the capital name <capital> is correct
+
+    Examples:
+    | isoCode | capital     |
+    | CO      | Bogota      |
+    | MX      | Mexico City |
+    | AR      | Buenos Aires|
